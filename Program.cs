e846594a153301c24d802c33a0dff826449d9315
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-10-03 17:50:48 +0200

    || app.Environment.IsProduction()

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
